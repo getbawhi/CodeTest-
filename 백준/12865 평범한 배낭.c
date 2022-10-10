14cbd6decf1ef995dd5a36730ae13d05710f831b
Getbawhi <95276794+getbawhi@users.noreply.github.com>
2022-10-10 15:13:04 +0900

    Create 12865 평범한 배낭.c

---
+++ 백준/12865 평범한 배낭.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main() {
+
+	int n = 0, maxw = 0;
+	int tmp = 0;
+	int table[101][1000001] = { 0 };
+
+	int value[101] = { 0 };
+	int weight[101] = { 0 };
+
+	scanf("%d %d", &n, &maxw);
+
+	for (int i = 1; i <= n; i++)
+	{
+		scanf("%d %d", &weight[i], &value[i]);
+	}
+
+	for (int i = 1; i <= n; i++)
+	{
+
+		for (int j = 1; j <= maxw; j++)
+		{
+			if (weight[i] > j)
+				table[i][j] = table[i - 1][j];
+			else 
+			{
+				tmp = value[i] + table[i-1][j - weight[i]];
+				if (tmp > table[i - 1][j])
+					table[i][j] = tmp;
+				else
+					table[i][j] = table[i - 1][j];
+
+			}
+
+		}
+
+
+	}
+
+	printf("%d", table[n][maxw]);
+
+	return 0;
+
+
+
+}
